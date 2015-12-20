f5c53563dc6c58254a9e87ef47e330ab322f0dea
Pavel Sereda <ideadboom@mail.ru>
2015-12-20 16:37:23 +0300

    5 commit

--- Flowers/src/main/java/com/deadboom/flowers/MainActivity.java
+++ Flowers/src/main/java/com/deadboom/flowers/MainActivity.java
@@ -10,8 +10,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        //5
+        //
         setContentView(R.layout.activity_main);
     }
 }
